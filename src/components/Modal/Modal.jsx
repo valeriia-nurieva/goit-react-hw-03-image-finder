f1cc090ea4509848bbeb9959ef2d6d90d19f7178
valeriia-nurieva <valerka.0906@gmail.com>
2022-12-11 07:58:50 +0200

    fix modal

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,31 +1,39 @@
 import Modal from 'react-modal';
+import { GrFormClose } from 'react-icons/gr';
+import { ButtonClose } from './Modal.styled';
 import PropTypes from 'prop-types';
 const modalStyles = {
   content: {
-    top: '50%',
-    left: '50%',
-    right: 'auto',
-    bottom: 'auto',
-    marginRight: '-50%',
-    transform: 'translate(-50%, -50%)',
+    // top: '(50%)',
+    // left: '50%',
+    // right: 'auto',
+    // bottom: 'auto',
+    // marginRight: '-50%',
+    // transform: 'translate(-50%, -50%)',
+    top: '100px',
+    left: '15px',
+    bottom: '15px',
+    right: '15px',
   },
 };
 Modal.setAppElement('#root');
 
 export const ModalImage = ({ selectImage, resetImage }) => {
-    return (
-        <Modal
-            isOpen={Boolean(selectImage)}
-            onRequestClose={resetImage}
-            style={modalStyles}
-        >
-            <button onClick={resetImage}>close</button>
-            <img src={selectImage} alt="Large" />
-        </Modal>
-    )
+  return (
+    <Modal
+      isOpen={Boolean(selectImage)}
+      onRequestClose={resetImage}
+      style={modalStyles}
+    >
+      <ButtonClose onClick={resetImage}>
+        <GrFormClose />
+      </ButtonClose>
+      <img src={selectImage} alt="Large" />
+    </Modal>
+  );
 };
 
 Modal.propTypes = {
-    selectImage: PropTypes.string,
-    resetImage: PropTypes.func,
+  selectImage: PropTypes.string,
+  resetImage: PropTypes.func,
 };
---
+++ src/components/Modal/Modal.styled.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const ButtonClose = styled.button`
+  border: none;
+  background-color: transparent;
+  outline: none;
+  display: block;
+  margin-left: auto;
+  padding: 0;
+`;
